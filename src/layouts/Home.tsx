7420ca76b936a0a6b7ff9a3171a34ff3e99554f3
Mathilde Rolland <rolland.mmathilde@gmail.com>
2021-12-20 08:23:57 +0100

    Set up of the base architecture of the platform

---
+++ src/layouts/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// == STYLES
+import '../assets/styles/modules/home.scss';
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <p>Ptit coucou de la Home!</p>
+        </div>
+    )
+}
